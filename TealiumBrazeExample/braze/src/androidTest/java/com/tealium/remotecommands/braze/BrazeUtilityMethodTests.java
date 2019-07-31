24d2ce3beaf219d50999ddb22ea0327ad4ccfb49
jameskeith <james.keith@tealium.com>
2019-07-31 16:21:38 +0100

    sample app + source

---
+++ TealiumBrazeExample/braze/src/androidTest/java/com/tealium/remotecommands/braze/BrazeUtilityMethodTests.java
@@ -0,0 +1,293 @@
+package com.tealium.remotecommands.braze;
+
+import androidx.test.runner.AndroidJUnit4;
+
+import com.appboy.enums.Gender;
+import com.appboy.models.outgoing.AppboyProperties;
+import com.tealium.collect.attribute.AudienceAttribute;
+import com.tealium.collect.attribute.BadgeAttribute;
+import com.tealium.collect.attribute.DateAttribute;
+import com.tealium.collect.attribute.FlagAttribute;
+import com.tealium.collect.attribute.MetricAttribute;
+import com.tealium.collect.attribute.PropertyAttribute;
+
+import junit.framework.Assert;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.math.BigDecimal;
+
+@RunWith(AndroidJUnit4.class)
+public class BrazeUtilityMethodTests {
+
+    public BrazeUtilityMethodTests() { }
+
+    @Test
+    public void genderStringToEnumTest(){
+
+        // Male
+        Assert.assertEquals(BrazeUtils.getGenderEnumFromString("Male"), Gender.MALE);
+        Assert.assertEquals(BrazeUtils.getGenderEnumFromString("m"), Gender.MALE);
+
+        // Female
+        Assert.assertEquals(BrazeUtils.getGenderEnumFromString("female"), Gender.FEMALE);
+        Assert.assertEquals(BrazeUtils.getGenderEnumFromString("f"), Gender.FEMALE);
+
+        // Other
+        Assert.assertEquals(BrazeUtils.getGenderEnumFromString("otHer"), Gender.OTHER);
+        Assert.assertEquals(BrazeUtils.getGenderEnumFromString("o"), Gender.OTHER);
+
+        // N/A
+        Assert.assertEquals(BrazeUtils.getGenderEnumFromString("not_applicable"), Gender.NOT_APPLICABLE);
+        Assert.assertEquals(BrazeUtils.getGenderEnumFromString("na"), Gender.NOT_APPLICABLE);
+
+        // Prefer not to say
+        Assert.assertEquals(BrazeUtils.getGenderEnumFromString("prefer_NOT_to_say"), Gender.PREFER_NOT_TO_SAY);
+        Assert.assertEquals(BrazeUtils.getGenderEnumFromString("no"), Gender.PREFER_NOT_TO_SAY);
+
+        // Unknown
+        Assert.assertEquals(BrazeUtils.getGenderEnumFromString("unknown"), Gender.UNKNOWN);
+
+        // default
+        Assert.assertEquals(BrazeUtils.getGenderEnumFromString("UNEXPECTED VALUE"), null);
+    }
+
+    @Test
+    public void addCustomPropertyTests(){
+        Object stringValue = "test";
+        Object integerValue = 10;
+        Object doubleValue = 10.10;
+        Object booleanValue = true;
+        Object integerStringValue = "10";
+        Object doubleStringValue = "10.10";
+        Object booleanStringValue = "true";
+        // Object dateValue = ""; //TODO: get an example of the data in simple date format.
+
+        AppboyProperties props = new AppboyProperties();
+        props = BrazeUtils.addCustomProperty("stringValue", stringValue, props);
+        props = BrazeUtils.addCustomProperty("integerValue", integerValue, props);
+        props = BrazeUtils.addCustomProperty("doubleValue", doubleValue, props);
+        props = BrazeUtils.addCustomProperty("booleanValue", booleanValue, props);
+        props = BrazeUtils.addCustomProperty("integerStringValue", integerStringValue, props);
+        props = BrazeUtils.addCustomProperty("doubleStringValue", doubleStringValue, props);
+        props = BrazeUtils.addCustomProperty("booleanStringValue", booleanStringValue, props);
+
+        JSONObject appboyPropsJson = props.forJsonPut();
+        try {
+            Assert.assertEquals(stringValue, appboyPropsJson.getString("stringValue"));
+            Assert.assertTrue(appboyPropsJson.get("stringValue") instanceof  String);
+            Assert.assertEquals(integerValue, appboyPropsJson.getInt("integerValue"));
+            Assert.assertTrue(appboyPropsJson.get("integerValue") instanceof  Integer);
+            Assert.assertEquals(doubleValue, appboyPropsJson.getDouble("doubleValue"));
+            Assert.assertTrue(appboyPropsJson.get("doubleValue") instanceof  Double);
+            Assert.assertEquals(booleanValue, appboyPropsJson.getBoolean("booleanValue"));
+            Assert.assertTrue(appboyPropsJson.get("booleanValue") instanceof  Boolean);
+
+            /*
+            * At the time of writing, the Android SDK will stringify values in a HashMap such that
+            * the native type is lost. The method being tested here will attempt to recover that.
+            * As a result the expected types should be integer/double/booolean despite the value
+            * that was put in, was actually a string.
+            * */
+            Assert.assertEquals(integerValue, appboyPropsJson.getInt("integerStringValue"));
+            Assert.assertTrue(appboyPropsJson.get("integerStringValue") instanceof  Integer);
+            Assert.assertEquals(doubleValue, appboyPropsJson.getDouble("doubleStringValue"));
+            Assert.assertTrue(appboyPropsJson.get("doubleStringValue") instanceof  Double);
+            Assert.assertEquals(booleanValue, appboyPropsJson.getBoolean("booleanStringValue"));
+            Assert.assertTrue(appboyPropsJson.get("booleanStringValue") instanceof  Boolean);
+
+        }catch(JSONException jex){
+            Assert.fail();
+        }
+
+        AppboyProperties props2 = BrazeUtils.addCustomProperty("stringValue", stringValue);
+        JSONObject appboyPropsJson2 = props2.forJsonPut();
+        try {
+            // Shorthand method should generate a new AppboyProperties if one isn't suppplied
+            Assert.assertNotNull(props2);
+
+            Assert.assertEquals(stringValue, appboyPropsJson2.getString("stringValue"));
+            Assert.assertTrue(appboyPropsJson2.get("stringValue") instanceof  String);
+
+        }catch(JSONException jex){
+            Assert.fail();
+        }
+    }
+
+
+
+    @Test
+    public void visitorAttributesTests(){
+        String id = "101";
+        AudienceAttribute audienceAttribute = new AudienceAttribute(id, "My Audience Attribute");
+        BadgeAttribute badgeAttribute = new BadgeAttribute(id);
+        MetricAttribute metricAttribute = new MetricAttribute(id, 1.1);
+        PropertyAttribute propertyAttribute = new PropertyAttribute(id, "value");
+        FlagAttribute flagAttribute = new FlagAttribute(id, true);
+        DateAttribute dateAttribute = new DateAttribute(id, 230918L);
+
+
+        Assert.assertEquals(BrazeUtils.getAttributeKeyName(audienceAttribute), "tealium_audience_my_audience_attribute");
+        Assert.assertEquals(BrazeUtils.getAttributeKeyName(badgeAttribute), "tealium_badge_101");
+        Assert.assertEquals(BrazeUtils.getAttributeKeyName(metricAttribute), "tealium_metric_101");
+        Assert.assertEquals(BrazeUtils.getAttributeKeyName(propertyAttribute), "tealium_property_101");
+        Assert.assertEquals(BrazeUtils.getAttributeKeyName(flagAttribute), "tealium_flag_101");
+        Assert.assertEquals(BrazeUtils.getAttributeKeyName(dateAttribute), "tealium_date_101");
+    }
+
+    @Test
+    public void jsonKeyHasValueTests(){
+        JSONObject json = new JSONObject();
+        try {
+            json.put("string_key", "string");
+            json.put("null_key", null);
+
+            Assert.assertTrue(BrazeUtils.keyHasValue(json, "string_key"));
+            Assert.assertFalse(BrazeUtils.keyHasValue(json, "null_key"));
+            Assert.assertFalse(BrazeUtils.keyHasValue(json, "non_existent_key"));
+
+        }catch (Exception e){
+            Assert.fail();
+        }
+    }
+
+    @Test
+    public void isNullOrEmptyTests(){
+        String populatedString = "Some String";
+        String emptyString = "";
+        JSONObject populatedJsonObject = new JSONObject();
+        JSONArray populatedJsonArray = new JSONArray();
+        JSONObject emptyJsonObject = new JSONObject();
+        JSONArray emptyJsonArray = new JSONArray();
+        try {
+            populatedJsonObject.put("anyKey", "anyvalue");
+            populatedJsonArray.put("anyvalue");
+
+            // Strings
+            Assert.assertFalse(BrazeUtils.isNullOrEmpty(populatedString));
+            Assert.assertTrue(BrazeUtils.isNullOrEmpty(emptyString));
+            Assert.assertTrue(BrazeUtils.isNullOrEmpty((String)null));
+            // JSONObjects
+            Assert.assertFalse(BrazeUtils.isNullOrEmpty(populatedJsonObject));
+            Assert.assertTrue(BrazeUtils.isNullOrEmpty(emptyJsonObject));
+            Assert.assertTrue(BrazeUtils.isNullOrEmpty((JSONObject) null));
+            // JSONArrays
+            Assert.assertFalse(BrazeUtils.isNullOrEmpty(populatedJsonArray));
+            Assert.assertTrue(BrazeUtils.isNullOrEmpty(emptyJsonArray));
+            Assert.assertTrue(BrazeUtils.isNullOrEmpty((JSONArray) null));
+
+        }catch (Exception e){
+            Assert.fail();
+        }
+    }
+
+    @Test
+    public void jsonArrayToStringArrayTests(){
+        JSONArray expectedArray = new JSONArray();
+        expectedArray.put("value1");
+        expectedArray.put("value2");
+
+        JSONArray unexpectedArray = new JSONArray();
+        unexpectedArray.put("value1");
+        unexpectedArray.put(10);
+        unexpectedArray.put(false);
+
+        String[] expectedResult = BrazeUtils.getStringArrayFromJson(expectedArray);
+        String[] unexpectedResult = BrazeUtils.getStringArrayFromJson(unexpectedArray);
+
+        Assert.assertNotNull(expectedArray);
+        Assert.assertNotNull(unexpectedArray);
+
+        Assert.assertTrue(expectedResult.length == expectedArray.length());
+        Assert.assertTrue(unexpectedResult.length == unexpectedArray.length());
+
+        Assert.assertTrue(expectedResult[0].equals("value1") && expectedResult[1].equals("value2"));
+        Assert.assertTrue(unexpectedResult[0].equals("value1") && unexpectedResult[1].equals("10") && unexpectedResult[2].equals("false"));
+    }
+
+    @Test
+    public void jsonArrayToIntegerArrayTests(){
+        JSONArray expectedArray = new JSONArray();
+        expectedArray.put(10);
+        expectedArray.put(20);
+
+        JSONArray unexpectedArray = new JSONArray();
+        unexpectedArray.put("value1");
+        unexpectedArray.put(10);
+        unexpectedArray.put(false);
+
+        Integer[] expectedResult = BrazeUtils.getIntegerArrayFromJson(expectedArray);
+        Integer[] unexpectedResult = BrazeUtils.getIntegerArrayFromJson(unexpectedArray);
+
+        Assert.assertNotNull(expectedArray);
+        Assert.assertNotNull(unexpectedArray);
+
+        Assert.assertTrue(expectedResult.length == expectedArray.length());
+        Assert.assertTrue(unexpectedResult.length == unexpectedArray.length());
+
+        Assert.assertTrue(expectedResult[0] == 10 && expectedResult[1] == 20);
+        Assert.assertTrue(unexpectedResult[0] == 1 && unexpectedResult[1] == 10 && unexpectedResult[2] == 1);
+    }
+
+    @Test
+    public void jsonArrayToBigDecimalArrayTests(){
+        JSONArray expectedArray = new JSONArray();
+        expectedArray.put(10);
+        expectedArray.put(20);
+
+        JSONArray unexpectedArray = new JSONArray();
+        unexpectedArray.put("value1");
+        unexpectedArray.put(10);
+        unexpectedArray.put(false);
+
+        BigDecimal[] expectedResult = BrazeUtils.getBigDecimalArrayFromJson(expectedArray);
+        BigDecimal[] unexpectedResult = BrazeUtils.getBigDecimalArrayFromJson(unexpectedArray);
+
+        Assert.assertNotNull(expectedArray);
+        Assert.assertNotNull(unexpectedArray);
+
+        Assert.assertTrue(expectedResult.length == expectedArray.length());
+        Assert.assertTrue(unexpectedResult.length == unexpectedArray.length());
+
+        Assert.assertTrue(expectedResult[0].equals(new BigDecimal(10)) && expectedResult[1].equals(new BigDecimal(20)));
+        Assert.assertTrue(unexpectedResult[0].equals(new BigDecimal(0)) && unexpectedResult[1].equals(new BigDecimal(10)) && unexpectedResult[2].equals(new BigDecimal(0)));
+    }
+
+    @Test
+    public void jsonArrayToJSONObjectArrayTests(){
+        JSONArray expectedArray = new JSONArray();
+        JSONObject filledObject = new JSONObject();
+        JSONObject emptyObject = new JSONObject();
+        try {
+            filledObject.put("key", "value");
+            filledObject.put("anotherkey", "anotherValue");
+
+            expectedArray.put(filledObject);
+            expectedArray.put(emptyObject);
+
+            JSONArray unexpectedArray = new JSONArray();
+            unexpectedArray.put(filledObject);
+            unexpectedArray.put(10);
+            unexpectedArray.put(false);
+
+            JSONObject[] expectedResult = BrazeUtils.getJSONObjectArrayFromJson(expectedArray);
+            JSONObject[] unexpectedResult = BrazeUtils.getJSONObjectArrayFromJson(unexpectedArray);
+
+            Assert.assertNotNull(expectedArray);
+            Assert.assertNotNull(unexpectedArray);
+
+            Assert.assertTrue(expectedResult.length == expectedArray.length());
+            Assert.assertTrue(unexpectedResult.length == unexpectedArray.length());
+
+            Assert.assertTrue(expectedResult[0].toString().equals(filledObject.toString()) && expectedResult[1].toString().equals(emptyObject.toString()));
+            Assert.assertTrue(unexpectedResult[0].toString().equals(filledObject.toString()) && unexpectedResult[1].toString().equals(emptyObject.toString()) && unexpectedResult[2].toString().equals(emptyObject.toString()));
+
+        }catch(Exception e){
+            Assert.fail();
+        }
+    }
+}
---
+++ TealiumBrazeExample/braze/src/main/java/com/tealium/remotecommands/braze/BrazeUtils.java
@@ -0,0 +1,344 @@
+package com.tealium.remotecommands.braze;
+
+import android.util.Log;
+
+import com.appboy.enums.Gender;
+import com.appboy.models.outgoing.AppboyProperties;
+import com.tealium.collect.attribute.AudienceAttribute;
+import com.tealium.collect.attribute.BadgeAttribute;
+import com.tealium.collect.attribute.BaseAttribute;
+import com.tealium.collect.attribute.DateAttribute;
+import com.tealium.collect.attribute.FlagAttribute;
+import com.tealium.collect.attribute.MetricAttribute;
+import com.tealium.collect.attribute.PropertyAttribute;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.math.BigDecimal;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Iterator;
+
+class BrazeUtils {
+
+    /**
+     * The Format of any Dates that were sent into the WebView as a native java.util.Date, will be
+     * returned to the RemoteCommand in the following date format. This is a static property to
+     * easily help conversion back into a native Date type.
+     */
+    public static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("E MMM dd HH:mm:ss z yyyy");
+
+
+    /**
+     * At the time of writing, the Android SDK will stringify values in a HashMap such that
+     * the native type is lost. The method being tested here will attempt to recover that.
+     * As a result the expected types should be integer/double/booolean etc despite the value
+     * that was sent in the event might actually have been a string.
+     * <p>
+     * This is a helper method that will take
+     *
+     * @param key        - name of the custom property to add.
+     * @param data       - value to add to the custom property
+     * @param properties - an existing AppboyProperties object to add this key-value pair to. If
+     *                   null, then a new AppboyProperties object will be created to be returned
+     * @return
+     */
+    public static AppboyProperties addCustomProperty(String key, Object data, AppboyProperties properties) {
+        if (properties == null) {
+            Log.d(BrazeConstants.TAG, "Creating new AppboyProperties");
+            properties = new AppboyProperties();
+        }
+
+        if (data instanceof String) {
+            try {
+                properties = addCustomProperty(key, Integer.parseInt((String) data), properties);
+                return properties;
+            } catch (NumberFormatException ignored) {
+
+            }
+            try {
+                properties = addCustomProperty(key, Double.parseDouble((String) data), properties);
+                return properties;
+            } catch (NumberFormatException ignored) {
+
+            }
+            try {
+                properties = addCustomProperty(key, DATE_FORMAT.parse((String) data), properties);
+                return properties;
+            } catch (ParseException ignored) {
+
+            }
+            if (((String) data).equalsIgnoreCase("true") || ((String) data).equalsIgnoreCase("false")) {
+                properties = addCustomProperty(key, Boolean.parseBoolean((String) data), properties);
+                return properties;
+            } else {
+                properties.addProperty(key, (String) data);
+            }
+
+        } else if (data instanceof Integer) {
+            properties.addProperty(key, (Integer) data);
+        } else if (data instanceof Double) {
+            properties.addProperty(key, (Double) data);
+        } else if (data instanceof Boolean) {
+            properties.addProperty(key, (Boolean) data);
+        } else if (data instanceof Date) {
+            properties.addProperty(key, (Date) data);
+        }
+
+        return properties;
+    }
+
+    /**
+     * Short-hand method for calling addCustomProperty, generating a new AppboyProperties object at
+     * the same time.
+     *
+     * @param key  - name of the custom property to add.
+     * @param data - value to add to the custom property
+     * @return
+     */
+    public static AppboyProperties addCustomProperty(String key, Object data) {
+        return addCustomProperty(key, data, null);
+    }
+
+    /**
+     * Helper method to translate a JSONObject of key-value pairs into an AppboyProperties object.
+     * The values in the JSONObject should only be supported types for the AppboyProperties class,
+     * which at the time of writing is only String, Integer, Double, Date and Boolean
+     *
+     * @param customProperties - JSONObject of Key-Value pairs.
+     * @return AppboyProperties containing the Key-Value pairs supplied
+     */
+    public static AppboyProperties extractCustomProperties(JSONObject customProperties) {
+        AppboyProperties props = new AppboyProperties();
+        if (customProperties != null) {
+            try {
+                // add the provided custom properties into the new payload object
+                Iterator<String> iterator = customProperties.keys();
+                while (iterator.hasNext()) {
+                    String key = iterator.next();
+                    props = BrazeUtils.addCustomProperty(key, customProperties.get(key), props);
+                }
+
+            } catch (JSONException jex) {
+                Log.w(BrazeConstants.TAG, "Failed to extract custom_attributes from JSON", jex);
+            } catch (NullPointerException npe) {
+                Log.w(BrazeConstants.TAG, "Object is null.", npe);
+            }
+        }
+        return props;
+    }
+
+
+    /**
+     * Helper method to convert a Visitor Attribute into a String representation. For Audience
+     * attributes, the name will be lowercased and any whitespace converted into underscores, where
+     * the format will be:
+     * tealium_{attribute_type}_{audience_name}
+     * e.g. tealium_audience_my_audience_name
+     * <p>
+     * For all other attribute types, the Attribute ID will be used instead:
+     * e.g tealium_badge_105
+     *
+     * @param attribute
+     * @return
+     */
+    public static String getAttributeKeyName(BaseAttribute attribute) {
+        String value = "tealium_";
+
+        if (attribute instanceof AudienceAttribute) {
+            value += "audience_" + ((AudienceAttribute) attribute).getName().toLowerCase().replaceAll(" ", "_");
+        } else if (attribute instanceof BadgeAttribute) {
+            value += "badge_" + attribute.getId();
+        } else if (attribute instanceof MetricAttribute) {
+            value += "metric_" + attribute.getId();
+        } else if (attribute instanceof FlagAttribute) {
+            value += "flag_" + attribute.getId();
+        } else if (attribute instanceof DateAttribute) {
+            value += "date_" + attribute.getId();
+        } else if (attribute instanceof PropertyAttribute) {
+            value += "property_" + attribute.getId();
+        }
+
+        return value;
+    }
+
+    /**
+     * Helper method to determine whether a particular key in a JSONObject is present AND also is
+     * not a null value.
+     *
+     * @param json - the JSONObject to inspect
+     * @param key  - the key in the JSONObject to check existence and value
+     * @return
+     */
+    static boolean keyHasValue(JSONObject json, String key) {
+        return (json != null && json.has(key) && !json.isNull(key));
+    }
+
+    /**
+     * Helper to convert string representation of a Gender into the required.
+     * <p>
+     * (m)ale = Gender.MALE
+     * (f)emale = Gender.FEMALE
+     * (o)ther = Gender.OTHER
+     * na/not_applicable = Gender.NOT_APPLICABLE
+     * no/prefer_not_to_say = Gender.PREFER_NOT_TO_SAY
+     * unknown = Gender.UNKNOWN
+     * <p>
+     * else return null
+     *
+     * @param genderString
+     * @return The Gender enum if found, or null
+     */
+    public static Gender getGenderEnumFromString(String genderString) {
+        Gender genderEnum;
+        switch (genderString.toLowerCase()) {
+            case "female":
+            case "f":
+                genderEnum = Gender.FEMALE;
+                break;
+            case "male":
+            case "m":
+                genderEnum = Gender.MALE;
+                break;
+            case "other":
+            case "o":
+                genderEnum = Gender.OTHER;
+                break;
+            case "not_applicable":
+            case "na":
+                genderEnum = Gender.NOT_APPLICABLE;
+                break;
+            case "prefer_not_to_say":
+            case "no":
+                genderEnum = Gender.PREFER_NOT_TO_SAY;
+                break;
+            case "unknown":
+                genderEnum = Gender.UNKNOWN;
+                break;
+            default:
+                genderEnum = null;
+                break;
+        }
+
+        return genderEnum;
+    }
+
+
+    /**
+     * Helper to convert a JSONArray to and array of Strings
+     *
+     * @param jsonArray
+     * @return
+     */
+    static String[] getStringArrayFromJson(JSONArray jsonArray) {
+        String[] returnData;
+        if (isNullOrEmpty(jsonArray)) {
+            returnData = new String[0];
+        } else {
+            returnData = new String[jsonArray.length()];
+            for (int i = 0; i < jsonArray.length(); i++) {
+                returnData[i] = jsonArray.optString(i, "");
+            }
+        }
+
+        return returnData;
+    }
+
+    /**
+     * Helper to convert a JSONArray to and array of Integers
+     *
+     * @param jsonArray
+     * @return
+     */
+    static Integer[] getIntegerArrayFromJson(JSONArray jsonArray) {
+        Integer[] returnData;
+        if (isNullOrEmpty(jsonArray)) {
+            returnData = new Integer[0];
+        } else {
+            returnData = new Integer[jsonArray.length()];
+            for (int i = 0; i < jsonArray.length(); i++) {
+                returnData[i] = jsonArray.optInt(i, 1);
+            }
+        }
+
+        return returnData;
+    }
+
+    /**
+     * Helper to convert a JSONArray to and array of BigDecimals
+     *
+     * @param jsonArray
+     * @return
+     */
+    static BigDecimal[] getBigDecimalArrayFromJson(JSONArray jsonArray) {
+        BigDecimal[] returnData;
+        if (isNullOrEmpty(jsonArray)) {
+            returnData = new BigDecimal[0];
+        } else {
+            returnData = new BigDecimal[jsonArray.length()];
+            for (int i = 0; i < jsonArray.length(); i++) {
+                returnData[i] = new BigDecimal(jsonArray.optDouble(i, 0));
+            }
+        }
+
+        return returnData;
+    }
+
+    /**
+     * Helper to convert a JSONArray to and array of JSONObjects
+     *
+     * @param jsonArray
+     * @return
+     */
+    static JSONObject[] getJSONObjectArrayFromJson(JSONArray jsonArray) {
+        JSONObject[] returnData;
+        if (isNullOrEmpty(jsonArray)) {
+            returnData = new JSONObject[0];
+        } else {
+            returnData = new JSONObject[jsonArray.length()];
+            for (int i = 0; i < jsonArray.length(); i++) {
+                try {
+                    String jsonString = jsonArray.optString(i, "{}");
+                    returnData[i] = new JSONObject(jsonString);
+                } catch (JSONException jsEx) {
+                    returnData[i] = new JSONObject();
+                }
+            }
+        }
+
+        return returnData;
+    }
+
+    /**
+     * Helper to determine if the JSONArray is null, or has no entries.
+     *
+     * @param jsonArray
+     * @return
+     */
+    static boolean isNullOrEmpty(JSONArray jsonArray) {
+        return jsonArray == null || jsonArray.length() == 0;
+    }
+
+    /**
+     * Helper to determine if the JSONObject is null, or has no keys.
+     *
+     * @param jsonObject
+     * @return
+     */
+    static boolean isNullOrEmpty(JSONObject jsonObject) {
+        return jsonObject == null || jsonObject.length() == 0;
+    }
+
+    /**
+     * Helper to determine if the String is null, or is empty.
+     *
+     * @param string
+     * @return
+     */
+    static boolean isNullOrEmpty(String string) {
+        return string == null || string.isEmpty();
+    }
+}
